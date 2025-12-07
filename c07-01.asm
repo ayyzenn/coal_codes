aedc41bd1b68b15ca7b33669ee6cd984ac9075c5
ayyzenn <saad.ahmad1024@gmail.com>
2025-12-07 19:37:55 +0500

    removing c07-04.asm and updating the c071-01.asm file

--- c07-01.asm
+++ c07-01.asm
@@ -2,10 +2,10 @@
 
 mov  ax, 0xb800         ; video memory base
 mov  es, ax             ; cannot move to es through IMM
-mov  di, 0              ; top left location 
+mov  di, 0              ; top left location
 
-nextpos: 
-    mov  word [es:di], 0x0776       ; 0x07 -- full white  (try 41)
+nextpos:
+    mov  word [es:di], 0x0720       ; 0x07 -- full white  (try 41)
                                     ; 0x20 is the space character
     add  di, 2
     cmp  di, 4000
@@ -13,4 +13,4 @@ nextpos:
 
     mov  ax, 0x4c00
     int  0x21
-    
+
